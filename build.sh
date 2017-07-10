4c6841accecc881d344fef046fa530fe78d6d3f5
kiksen1987 <joshua.ryder@outlook.com>
2017-07-10 23:34:48 +0200

    mono and core

--- build.sh
+++ build.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build && dotnet pack
-dotnet pack ./TCB -c Release -o ./artifacts --version-suffix=$revision
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+dotnet restore
+
+# Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
+# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+
+dotnet test ./UnitTest/UnitTest.csproj -c Release -f netcoreapp1.0
+
+# Instead, run directly with mono for the full .net version 
+dotnet build ./TCB/TCB.csproj -c Release -f net451
+
+mono \  
+./TCB/bin/Release/net451/*/dotnet-test-xunit.exe \
+./TCB/bin/Release/net451/*/TEST_PROJECT_NAME.dll
+
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
+
+dotnet pack ./src/PROJECT_NAME -c Release -o ./artifacts --version-suffix=$revision 
