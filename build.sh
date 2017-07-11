082e83192f789890db08495db0446c190825f0e7
kiksen1987 <joshua.ryder@outlook.com>
2017-07-11 21:08:05 +0200

    Fixed versioning

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 dotnet restore && dotnet build && dotnet pack
-dotnet pack ./TCB -c Release -o ./artifacts --version-suffix=$revision
-ls
+
 cd TCB
-ls
 dotnet pack /p:PackageVersion=1.0.7.$TRAVIS_BUILD_NUMBER --configuration Release
-cd bin
-ls
-cd 
-Release
-ls
-nuget push /home/travis/build/kiksen1987/HttpCircuitBreaker/TCB/bin/Debug/Topswagcode.http.circuitbreaker.*.nupkg -ApiKey $NUGET_API_KEY -Source https://www.nuget.org -Verbosity detailed
+
+nuget push /home/travis/build/kiksen1987/HttpCircuitBreaker/TCB/bin/Release/Topswagcode.http.circuitbreaker.*.nupkg -ApiKey $NUGET_API_KEY -Source https://www.nuget.org -Verbosity detailed
