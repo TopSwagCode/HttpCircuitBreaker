193570ccabb0ed3f21f79f9e0fff2840cfd94ce6
Joshua Jesper Krægpøth Ryder <joshua.ryder@outlook.com>
2017-07-11 10:44:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ ls
 cd TCB
 ls
 dotnet pack --configuration Release
-mono /usr/local/bin/nuget push ./bin/Release/TCB*.nupkg -ApiKey $NUGET_API_KEY -Source https://www.nuget.org -Verbosity detailed
+mono nuget push ./bin/Release/TCB*.nupkg -ApiKey $NUGET_API_KEY -Source https://www.nuget.org -Verbosity detailed
